7b6f6045be9655cebf5a75c259344e751c3d2601
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-04-18 02:33:18 +0330

    2

--- Formula/gptcommit.rb
+++ Formula/gptcommit.rb
@@ -2,7 +2,7 @@ class Gptcommit < Formula
   desc "A tool for creating commit messages using GPT-3"
   homepage "https://github.com/mazaheriMahdi/gptcommit"
   url "https://github.com/mazaheriMahdi/gptcommit/releases/download/v1.0.0-alpha/gptcommit-macos-v1-beta-x64.tar.gz"
-  sha256 "f71ef679087176675d21ebcea92210b899b1cf358d3891bb7f40925f"
+  sha256 "d8c54d0df54948a167cb7251af138bd4a0616ac99c15d674070d23f018d7afae"
   version "1.0.0-alpha"
   def install
     bin.install "gptcommit"
